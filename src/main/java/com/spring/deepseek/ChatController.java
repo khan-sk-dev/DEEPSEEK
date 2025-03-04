fe4f9e590782d24e7a6b33624d495391642cd603
sohaibkhan-007 <sohaibkhanatwork@gmail.com>
2025-03-04 15:31:03 +0530

    completed the code of DEEPSEEK API project

--- src/main/java/com/spring/deepseek/ChatController.java
+++ src/main/java/com/spring/deepseek/ChatController.java
@@ -2,10 +2,13 @@ package com.spring.deepseek;
 
 import java.util.Map;
 
+import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Flux;
@@ -21,12 +24,15 @@ public class ChatController {
     }
 
     @GetMapping("/ai/generate")
-    public Map<String, String> generate (@RequestParam(value = "message", defaultValue =  "tell me about yourself")){
+    public Map<String, String> generate(
+            @RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
         return Map.of("generation", this.chatModel.call(message));
     }
 
-    public  Flux<ChatResponse> generateStream(@RequestParam(value="message", defaultValue= "tell me about yourself")){
-        Prompt prompt  = new Prompt(new UserMessage(message));
+    @GetMapping("/ai/generateStream")
+    public Flux<ChatResponse> generateStream(
+            @RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        Prompt prompt = new Prompt(new UserMessage(message));
         return this.chatModel.stream(prompt);
     }
 }
